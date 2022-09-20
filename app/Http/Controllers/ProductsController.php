5e841ca3d2b90280f03f710d91f2e44210fc026e
obgov <102376675+obgov@users.noreply.github.com>
2022-09-20 22:06:51 +0300

    doing crud

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -8,12 +8,6 @@ use Illuminate\Http\Request;
 
 class ProductsController extends Controller
 {
-    protected Product $products;
-
-    public function __construct(Product $products)
-    {
-        $this->products = $products;
-    }
 
     public function index(): \Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View|\Illuminate\Contracts\Foundation\Application
     {
